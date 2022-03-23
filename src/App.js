7ed2c5970bd7935763a47e096028ec47222add72
twój login do GitHuba <96842769+MaksBernatowicz@users.noreply.github.com>
2022-03-23 15:27:46 +0100

    use states for boundaries of map and coordinates of center in Map.jsx and App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import Map from "./components/Map/Map";
 const App = () => {
     const [places, setPlaces] = useState([]);
 
+    const [coordinates, setCoordinates] = useState({lat: 0, lng: 0});
+    const [bounds, setBounds] = useState(null);
+
     useEffect(() => {
         getPlacesData()
             .then((data) => {
@@ -27,7 +30,11 @@ const App = () => {
                  <List/>
              </Grid>
              <Grid item xs={12} md={8}>
-                 <Map/>
+                 <Map
+                    setCoordinates={setCoordinates}
+                    setBounds={setBounds}
+                    coordinates={coordinates}
+                 />
              </Grid>
          </Grid>
      </>
