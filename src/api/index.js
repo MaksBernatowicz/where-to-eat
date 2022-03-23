a7186701121ee35ccaebb6333b343bedc5d4c0ad
twój login do GitHuba <96842769+MaksBernatowicz@users.noreply.github.com>
2022-03-23 16:20:44 +0100

    adding Google Places to List.jsx and App.js

--- src/api/index.js
+++ src/api/index.js
@@ -2,23 +2,20 @@ import axios from 'axios';
 
 const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
 
-const options = {
-    params: {
-        bl_latitude: '11.847676',
-        tr_latitude: '12.838442',
-        bl_longitude: '109.095887',
-        tr_longitude: '109.149359',
-        },
-    headers: {
-        'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
-        'X-RapidAPI-Key': '506cfb6df6msh9e9b42bac04ec4dp18654djsn46ae9a7d87e8'
-    }
-};
-
-
-export const getPlacesData = async () => {
+export const getPlacesData = async (sw, ne) => {
     try {
-        const {data: {data}} = await axios.get(URL, options);
+        const {data: {data}} = await axios.get(URL, {
+            params: {
+                bl_latitude: sw.lat,
+                tr_latitude: ne.lat,
+                bl_longitude: sw.lng,
+                tr_longitude: ne.lng,
+            },
+            headers: {
+                'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
+                'X-RapidAPI-Key': '506cfb6df6msh9e9b42bac04ec4dp18654djsn46ae9a7d87e8'
+            }
+        });
 
         return data;
     } catch (error) {
